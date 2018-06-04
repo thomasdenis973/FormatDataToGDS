ca24f99a9cab3b807c4d3b32ee282676e45a8f24
Thomas Denis <thomas.denis@ecofog.gf>
2018-06-04 09:34:26 -0300

    Commit enregistrement

--- R/PlotIkaDensity.R
+++ R/PlotIkaDensity.R
@@ -1,3 +1,12 @@
+#' Represent relatioship between IKA (i.e., Encounter rate, 10km walked length) and
+#' estimated density.
+#'
+#' @param data A FDataYGDS object created by CreateFDataYGDS function.
+#'
+#' @return Return a plot of estimated density (in x) and IKA (in x).
+#' @export
+#'
+#' @examples In progress
 PlotIkaDensity <- function(data){
   df<-data.frame(IKA=data$IKA,Density=data$Density)
   p <- ggplot2::ggplot(data=df, ggplot2::aes(x=IKA, y=Density)) +
